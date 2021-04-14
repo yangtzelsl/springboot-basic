e436b7c675417624b2a50c7dd6b6df6a2bfe94f2
luis.liu <yangtzelsl@126.com>
2021-04-14 10:09:19 +0800

    添加代码注释

--- src/main/java/com/yangtzelsl/security/SpringSecurityConfig.java
+++ src/main/java/com/yangtzelsl/security/SpringSecurityConfig.java
@@ -4,23 +4,21 @@ import com.yangtzelsl.security.authentication.MyAuthenctiationFailureHandler;
 import com.yangtzelsl.security.authentication.MyAuthenticationSuccessHandler;
 import com.yangtzelsl.security.authentication.MyLogoutSuccessHandler;
 import com.yangtzelsl.security.authentication.RestAuthenticationAccessDeniedHandler;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
+/**
+ * 处理用户信息获取逻辑 UserDetailsService
+ * 处理用户校验逻辑 UserDetails
+ * 处理密码加密解密逻辑 PasswordEncoder
+ */
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -39,21 +37,31 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private MyLogoutSuccessHandler myLogoutSuccessHandler;
 
+    /**
+     * http请求的安全处理
+     *
+     * @param httpSecurity
+     * @throws Exception
+     */
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
+        // 关闭 跨站请求伪造
         httpSecurity.csrf().disable();
+        // 开始请求权限配置
         httpSecurity.authorizeRequests()
+                // 下面配置的这些资源放行
                 .antMatchers("/login.html",
                         "/my/**",
                         "/treetable-lay/**",
                         "/xadmin/**",
                         "/ztree/**",
                         "/statics/**"
-                        )
+                )
                 .permitAll()
+                // 任何请求都要通过权限验证
                 .anyRequest()
                 .authenticated()
-                ;
+        ;
         //解决X-Frame-Options DENY问题
         httpSecurity.headers().frameOptions().sameOrigin();
         httpSecurity.formLogin()
@@ -61,7 +69,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginProcessingUrl("/login")
                 .successHandler(myAuthenticationSuccessHandler)
                 .failureHandler(myAuthenctiationFailureHandler)
-        .and().logout().permitAll().invalidateHttpSession(true).
+                .and().logout().permitAll().invalidateHttpSession(true).
                 deleteCookies("JSESSIONID").logoutSuccessHandler(myLogoutSuccessHandler)
         ;
         //异常处理
@@ -73,9 +81,17 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    /**
+     * 身份验证
+     *
+     * @param auth
+     * @throws Exception
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        auth.userDetailsService(userDetailsService)
+                // 密码加密
+                .passwordEncoder(passwordEncoder());
     }
 
 }
